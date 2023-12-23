b9b44d96e78fc10f848665d9bb6fa30ff6751634
Maxim Kim <kimmaxim69@gmail.com>
2023-12-23 14:08:50 +0300

    Закомментил версию браузера в conftest.py

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def browser():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    chrome_options.set_capability("browserVersion", "118")
+    # chrome_options.set_capability("browserVersion", "118")
     chrome_options.add_argument("--log-level=1")
     browser = webdriver.Chrome(options=chrome_options)
     browser.maximize_window()
